3c3fa752e8854f316db2b53022e364538931e12d
Suntipap Panrat <suntipap.p@kbtg.tech>
2021-02-08 11:51:06 +0700

    write

--- src/main/java/CircularBuffer.java
+++ src/main/java/CircularBuffer.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class CircularBuffer {
+    private String[] array;
+    private int writePosition;
+
+    public CircularBuffer(){
+        this(10);
+    }
+
+    public CircularBuffer(int size){
+        array = new String[size];
+        writePosition = 0;
+
+    }
+
+    public void write(String item){
+        if(array[writePosition]==null) {
+            array[writePosition] = item;
+        }
+    }
+
 }
